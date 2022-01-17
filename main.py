ffdebc47281a4eb5b01a56bbb9ea4cdb356e9f24
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-01-17 08:51:49 +0100

    Concat matrix-like structure

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from faulthandler import is_enabled
+import pdb
 import cv2
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -30,14 +32,41 @@ def save_frames(frames : list, output_folder : Path):
         cv2.imwrite(str(output_folder.joinpath(f"frame{str(i)}_{str(t)}.jpg")), frame)
     print(f"Saved {len(frames)} frames to {output_folder}")
     
+def concatenate_frames(list_2d):
+    return cv2.vconcat([cv2.hconcat(list_h) for list_h in list_2d])
+
+def preprocess_frames(frames):
+    """
+    Resize frames and transform then into concatenable list of lists
+    """
+    # TODO: resize all frames to a certain size
+    is_even = len(frames) % 2 == 0
+    middle = len(frames) // 2
+    if is_even:
+        return [frames[:middle], frames[middle:]]
+    else:
+        return [frames[:middle], frames[middle:len(frames) - 1]]
+
+def read_files(filenames):
+    return [cv2.imread(f) for f in filenames]
 
 def main():
-    input_folder, output_folder = get_io_folders()
-    video_path = input_folder.joinpath('big_buck_bunny_720p_5mb.mp4')
-    frames = frames_from_video(str(video_path))
-    save_frames(frames, output_folder)
+    # SAVE VIDEO TO FRAMES
+    # input_folder, output_folder = get_io_folders()
+    # video_path = input_folder.joinpath('big_buck_bunny_720p_5mb.mp4')
+    # frames = frames_from_video(str(video_path))
+    # save_frames(frames, output_folder)[]
 
-    
+
+
+    # CONCATENATE FRAMES FROM DISK
+    input_folder, output_folder = get_io_folders()
+    filenames = [str(f) for f in output_folder.glob('*.jpg')]
+    files = read_files(filenames)
+    cc = concatenate_frames(preprocess_frames(files))
+    plt.imshow(cc)
+    plt.show()
+   
 
 if __name__ == "__main__":
     main()
